4071d84b2fb2d24c03c9d36b3467921984522a64
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2024-08-21 11:06:57 -0400

    Added some temporary SEO titles and meta descriptions. Retrieved and are now being displayed on Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
 import Container from "./components/container";
 import Header from "./components/header";
+import Head from "next/head";
 
 export default async function Home() {
+  const res = await fetch(`${process.env.YOAST_API_URL}` );
+  const data = await res.json();
+  console.log(data);
   return (
     <>
+      <Head>
+        <title>{data.title}</title>
+        <meta name="description" content={data.description} />
+        <meta name="robots" content={data.robots} />
+        <meta name="keywords" content={data.keywords} />
+        <meta name="author" content={data.author} />
+      </Head>
       <Header />
       <Container />
     </>
