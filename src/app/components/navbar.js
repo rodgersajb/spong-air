f9050ebef294785720380c7b494ab0a0b9e74bc1
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2024-08-22 14:13:56 -0400

    adjusted nav menus for screens

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,8 +1,12 @@
-"use client"
+"use client";
 import Image from "next/image";
-
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+
+import { useState } from "react";
+
+import { IoClose } from "react-icons/io5";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const navLinks = [
   {
@@ -18,43 +22,97 @@ const navLinks = [
     label: "Services",
   },
   {
-    href: "/contact",
-    label: "Contact",
-  },
-  {
     href: "/posts",
     label: "Posts",
+  },
+  {
+    href: "/contact",
+    label: "Contact",
   },
 ];
 
 export default function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
   const pathname = usePathname();
   return (
-    <nav className="flex w-11/12 m-auto justify-between items-center h-32">
-      <Link href="/">
-        <Image
-          className=""
-          width={180}
-          height={100}
-          src="/images/Spongair-logo.JPG"
-          alt="logo"
-        />
-      </Link>
-      <div className="flex justify-center items-center gap-2">
-        <ul className="flex gap-6">
-          {navLinks.map((link) => (
-            <li
-              key={link.href}
-              className={`${
-                pathname === link.href ? "text-sky-950" : "text-sky-950"
-              }`}
-            >
-              <Link href={link.href}>{link.label}</Link>
-            </li>
-          ))}
-        </ul>
-        <button className="border-2 border-sky-950 p-2">Contact Us</button>
-      </div>
-    </nav>
-  );
+    
+     
+
+      
+      <main className="pb-16">
+        <nav className="fixed top-0 z-50 w-screen ">
+          <div className=" flex w-full items-center justify-between z-40  h-16">
+            <figure className="">
+              <Link href="/">
+                <Image
+                  className=""
+                  width={60}
+                  height={40}
+                  src="/images/Spongair-logo.JPG"
+                  alt="logo"
+                />
+              </Link>
+            </figure>
+            <div className="pr-2 z-40">
+              <button
+                onClick={toggleMenu}
+                className=" text-2xl  focus:outline-none "
+              >
+                {openMenu ? <IoClose /> : <RxHamburgerMenu />}
+              </button>
+            </div>
+          </div>
+          <ul
+            className={` min-h-svh w-full  flex flex-col justify-evenly items-center bg-slate-800 bg-opacity-75 origin-top z-30 ${
+              openMenu ? " animate-open-menu " : " animate-close-menu h-0"
+            }`}
+          >
+            {navLinks.map((link) => (
+              <li
+                key={link.href}
+                className={`text-white ${
+                  pathname === link.href ? "sm:text-sky-950" : "sm:text-sky-200"
+                }`}
+              >
+                <Link href={link.href}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </main>
+   
+  );  
 }
+
+ {
+   /* <nav className="hidden sm:flex sm:w-11/12 sm:m-auto sm:justify-between sm:items-center sm:h-32">
+        <Link href="/">
+          <Image
+            className=""
+            width={180}
+            height={100}
+            src="/images/Spongair-logo.JPG"
+            alt="logo"
+          />
+        </Link>
+        <div className="sm:flex sm:justify-center sm:items-center sm:gap-2">
+          <ul className="sm:flex sm:gap-6">
+            {navLinks.map((link) => (
+              <li
+                key={link.href}
+                className={`${
+                  pathname === link.href ? "sm:text-sky-950" : "sm:text-sky-950"
+                }`}
+              >
+                <Link href={link.href}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+         
+        </div>
+      </nav> */
+ }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,24 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": {
+            transform: "scaleY(1.2)",
+          },
+          "100%": { transform: "scaleY(1)" },
+        },
+        "close-menu": {
+          "0%": { transform: "scaleY(1)" },
+          "20%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(0)" },
+        },
+      },
+      animation: {
+        "open-menu": "open-menu 0.5s ease-in-out forwards",
+        "close-menu": "close-menu 0.5s ease-in-out forwards",
+      },
     },
   },
   plugins: [],
