360ef3e3a247e092b12396f0d373c8db873e14cf
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2024-10-25 13:47:47 -0400

    no slug either

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,25 +1,25 @@
-export async function generateStaticParams() {
-    const res = await fetch(`${process.env.WP_API_URL}/posts`);
-    const data = await res.json();
-    const paths = data.map((post) => ({
-        params: { slug: post.slug },
-    }));
-    return paths;
-}
+// export async function generateStaticParams() {
+//     const res = await fetch(`${process.env.WP_API_URL}/posts`);
+//     const data = await res.json();
+//     const paths = data.map((post) => ({
+//         params: { slug: post.slug },
+//     }));
+//     return paths;
+// }
 
-export default async function PostPage({ params }) {
+// export default async function PostPage({ params }) {
   
-  const res = await fetch(
-    `${process.env.WP_API_URL}/posts?slug=${params.slug}`
-  );
-  const data = await res.json();
+//   const res = await fetch(
+//     `${process.env.WP_API_URL}/posts?slug=${params.slug}`
+//   );
+//   const data = await res.json();
   
-  return (
-    <main>
-      <h1>{data[0].title.rendered}</h1>
-      <div>
-        <div dangerouslySetInnerHTML={{ __html: data[0].content.rendered }} />
-      </div>
-    </main>
-  );
-}
+//   return (
+//     <main>
+//       <h1>{data[0].title.rendered}</h1>
+//       <div>
+//         <div dangerouslySetInnerHTML={{ __html: data[0].content.rendered }} />
+//       </div>
+//     </main>
+//   );
+// }
