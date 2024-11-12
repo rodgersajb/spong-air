08a4e76409261326d7e7445c648010d82e696087
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2024-11-12 16:08:22 -0500

    lots of styling changes to the layout of services page

--- src/app/services/components/serviceList.jsx
+++ src/app/services/components/serviceList.jsx
@@ -2,9 +2,19 @@
 
 import Image from "next/image";
 import { useRef, useEffect } from "react";
-import { useInView, motion } from "framer-motion";
+import { useInView, motion, animate } from "framer-motion";
 
-export default function ServiceList({ service }) {
+const fadeInAnimation = {
+  initial: { opacity: 0, y: 100 },
+  animate: (index) => ({
+    opacity: 1,
+    y: 0,
+    transition: 0.6 * index,
+  }),
+};
+
+
+export default function ServiceList({ service, index }) {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true, margin: "0px 0px -200px 0px" });
 
@@ -15,25 +25,47 @@ export default function ServiceList({ service }) {
   }, [isInView]);
 
   return (
-    <main className="flex odd:flex-row-reverse w-[95%] m-auto justify-between  ">
-      <motion.div
-        ref={ref}
-        initial={{ opacity: 0, y: -40 }}
-        animate={isInView ? { opacity: 1, y: 0 } : {}}
-        transition={{ duration: 0.6 }}
-        className="w-[45%] flex flex-col justify-center rounded-xl bg-spongWhite  gap-8 relative after:content-[''] after:absolute after:h-[100%] after:w-[105%] after:bg-spongBlue after:-top-10 after:-right-10 after:rounded-xl after:-z-10 "
-      >
-        <h2 className="text-3xl">{service.service}</h2>
-        <p>{service.description}</p>
+    <motion.main
+      variants={fadeInAnimation}
+      initial="initial"
+      whileInView="animate"
+      viewport={{
+        once: true,
+      }}
+      custom={index}
+      className="flex flex-col  items-stretch w-[95%] m-auto justify-evenly bg-spongGrey lg:min-h-[350px] rounded-xl  "
+    >
+      <motion.div className="flex flex-col gap-12 w-[90%] m-auto   ">
+        <motion.figure className="bg-spongWhite w-9 grid place-items-center py-2 rounded-xl">
+          {service.icon}
+        </motion.figure>
+        <motion.h2
+          ref={ref}
+          initial={{ opacity: 0, y: -30 }}
+          animate={isInView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.6 }}
+          className="text-xl "
+        >
+          {service.service}
+        </motion.h2>
+        <motion.p
+          ref={ref}
+          initial={{ opacity: 0, y: -40 }}
+          animate={isInView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.6 }}
+          className="w-[75%] font-thin"
+        >
+          {service.description}
+        </motion.p>
       </motion.div>
-      <figure className="relative w-[45%] h-[300px]" >
+      {/* <figure className="relative h-[250px] w-[250px] first:w-[450px] ">
         <Image
           src={service.image}
-          alt={service.description}
+          alt={service.service}
           fill
-          className="rounded-2xl object-cover"
+          className="rounded-2xl object-cover aspect-square"
         />
-      </figure>
-    </main>
+      </figure> */}
+    </motion.main>
   );
 }
--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -5,14 +5,15 @@ import Link from "next/link";
 import { GiPaperPlane } from "react-icons/gi";
 import ServiceList from "./components/serviceList";
 
-const fadeInAnimation = {
-  initial: { opacity: 0, y: 100 },
-  animate: (index) => ({
-    opacity: 1,
-    y: 0,
-    transition: 0.6 * index,
-  }),
-};
+import { FaThermometerHalf } from "react-icons/fa";
+import { FaScrewdriverWrench } from "react-icons/fa6";
+import { WiWindy } from "react-icons/wi";
+import { PiPipeWrenchBold } from "react-icons/pi";
+import { AiOutlineAlert } from "react-icons/ai";
+import { FaLeaf } from "react-icons/fa";
+
+
+
 
 const imageAnimation = {
   initial: { opacity: 0, y: 50 },
@@ -25,31 +26,37 @@ const services = [
     service: "HVAC System Installation",
     description:
       "Expert installation of high-efficiency heating, ventilation, and air conditioning systems tailored to fit the unique needs of each space, ensuring optimal performance and energy savings.",
-      image: "/images/pexels-2.jpg",
+    icon: <FaThermometerHalf size={24} color="#143452" />,
   },
   {
     service: "Preventive Maintenance",
     description:
       "Comprehensive maintenance services that include detailed inspections, cleaning, and fine-tuning to keep HVAC systems running smoothly, extend their lifespan, and prevent unexpected breakdowns.",
-      image: "/images/pexels-3.jpg",
+    icon: <FaScrewdriverWrench size={24} color="#655853" />,
   },
   {
     service: "Indoor Air Quality Solutions",
     description:
       "Customized air filtration and purification solutions designed to enhance indoor air quality, reduce allergens, and promote a healthier environment for homes and businesses.",
-      image: "/images/tools.jpg",
+    icon: <WiWindy size={24} color="1e4055" />,
   },
   {
     service: "Ductwork Design and Installation",
     description:
       "Precise ductwork design and installation focused on maximizing airflow and energy efficiency, using quality materials and expert craftsmanship for seamless integration into any building layout.",
-      image: "/images/pexels-4.jpg",
+    icon: <PiPipeWrenchBold size={24} color="#143452" />,
   },
   {
-    service: "Emergency Repair Services",
+    service: "Emergency HVAC Repair",
     description:
-      "Quick-response emergency repair services to address sudden HVAC issues, with skilled technicians trained to diagnose and resolve problems effectively, minimizing downtime and ensuring consistent comfort.",
-      image: "/images/pexels-5.jpg",
+      "Rapid and reliable emergency repair services available 24/7 to address unexpected HVAC issues, ensuring minimal downtime and restoring comfort as quickly as possible.",
+    icon: <AiOutlineAlert size={24} color="#655853" />,
+  },
+  {
+    service: "Energy Efficiency Consultation",
+    description:
+      "Professional assessments and recommendations to improve energy efficiency in HVAC systems, helping clients reduce energy costs and achieve a more eco-friendly home or business.",
+    icon: <FaLeaf size={24} color="1e4055" />,
   },
 ];
 
@@ -83,20 +90,25 @@ export default function ServicesPage() {
               className="hidden lg:block absolute -top-[70px] right-0 left-0 "
             />
           </span>{" "}
-          technology ensure your HVAC systems are <span className="font-bold text-spongBlue pr-1">efficient,</span>
+          technology ensure your HVAC systems are{" "}
+          <span className="font-bold text-spongBlue pr-1">efficient,</span>
           <span className="italic text-spongBrown ">reliable,</span>
-          and <span className="font-semibold text-spongDarkBlue  relative before:content-[''] before:absolute before:h-[2px] before:w-full before:bg-spongDarkBlue before:bottom-0 ">prepared</span> for any climate.
+          and{" "}
+          <span className="font-semibold text-spongDarkBlue  relative before:content-[''] before:absolute before:h-[2px] before:w-full before:bg-spongDarkBlue before:bottom-0 ">
+            prepared
+          </span>{" "}
+          for any climate.
         </h2>
 
         <h4 className="text-xl pt-8 font-semibold lg:text-2xl ">
           What We <span className="text-spongBlue">Provide.</span>
         </h4>
-        
       </section>
-      <section className="flex flex-col gap-20 py-20">
+      <section className="grid grid-cols-3 grid-rows-2 gap-4 items-center w-[95%] m-auto">
         {services.map((service, index) => (
           <ServiceList key={index} service={service} />
-        ))}      
+        ))}
+       
       </section>
       <section className=" flex flex-col items-center justify-center gap-8 py-4">
         <h2 className="text-3xl text-spongDarkBlue ">Questions?</h2>
